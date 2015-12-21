0a62b56b486a5922d19ebc195cff3ce47c121f1b
Michael Smesnik <ms@tailored-apps.com>
2015-12-21 17:43:17 +0100

    add logger module

---
+++ logger.js
@@ -0,0 +1,60 @@
+"use strict"
+
+const moment = require('moment')
+const path = require('path')
+const winston = require('winston')
+
+const formatter = (opts) => `[${moment().format('YYYY-MM-DD HH:mm:ss.SSS')}] [${opts.level}] ${opts.message}`
+
+/**
+ * Creates a new logger instance
+ *
+ * @param {Object} cfg Configuration object
+ * @param {String} cfg.baseDir Base directory for all logfiles (optional, if file logging is disabled)
+ * @param {Object} cfg.destinations Logging destinations config object
+ * @param {Object} cfg.destinations.console Configuration object for console/stdout logging
+ * @param {Boolean} cfg.destinations.console.enable Enable or disable stdout logging
+ * @param {String} cfg.destinations.console.level Log level for stdout logs
+ * @param {Array.<{name: String, level: String}>|Boolean} cfg.destinations.files Configuration for file logging - file logging is disabled entirely if this is false
+ *
+ * @returns {Object}
+ */
+const createLogger = (cfg) => {
+    if (!cfg) {
+        cfg = {}
+    }
+
+    const destinations = cfg.destinations || {}
+
+    let logger = new winston.Logger()
+
+    if (destinations.console && destinations.console.enable) {
+        logger.add(winston.transports.Console, {
+            level: destinations.console.level,
+            formatter,
+        })
+    }
+
+    if (destinations.files && Array.isArray(destinations.files)) {
+        const baseDir = cfg.baseDir
+
+        destinations.files.forEach(f => {
+            const filename = path.resolve(baseDir, f.name)
+
+            logger.add(winston.transports.File, {
+                filename,
+                formatter,
+                level: f.level,
+                name: filename, // winston needs a unique name for each transport
+                json: false,
+            })
+        })
+    }
+
+    return logger
+}
+
+module.exports = {
+    createLogger,
+    formatter,
+}
