8a447fb04ac26ec131c5c4d3a918b63e566c6372
Michael Smesnik <ms@tailored-apps.com>
2016-08-02 14:30:01 +0200

    implement untested taskrunner class

--- src/taskrunner.js
+++ src/taskrunner.js
@@ -2,103 +2,84 @@
 
 import { getDuration } from './util'
 
-import TaskStats from 'models/TaskStats'
-import logger from 'logger'
-
-let runningTask
-/**
- * Simple queuing mechanism to avoid potential race conditions when running multiple tasks in parallel
- *
- * @type {Set}
- */
+// Simple queuing mechanism to avoid potential race conditions when running multple tasks in parallel
+let taskRunning = false
 let queue = new Set()
 
-class AbstractTaskRunner {
-  static async initiate (tasks) {
-    tasks.forEach(async ({ fn, interval, friendlyName }) => await new this.name(fn, interval, friendlyName).run())
-  }
-
-  /**
-   * Default task interval (used when no interval was specified explicitly)
-   *
-   * @returns {number}
-   */
-  static get defaultInterval () {
-    return 60
-  }
-
-  constructor (task, interval, friendlyName) {
+export default class AbstractTaskrunner {
+  constructor (task, interval, friendlyName = 'PeriodicTask') {
     this.task = task
-    this.interval = interval || this.constructor.defaultInterval
-    this.friendlyName = friendlyName || `PeriodicTask - ${this.task.name || 'unknown'}`
+    this.interval = interval
+    this.friendlyName = friendlyName
 
-    this._runs = 0
+    this.runCount = 0
 
-    logger.info('++++ Registering new periodic task "%s", running every %s sec.', this.friendlyName, this.interval)
+    this.logger.info('++++ Registering new periodic task "%s", running every %s sec.', this.friendlyName, this.interval)
+  }
+
+  get logger () {
+    throw new Error('Classes extending AbstractTaskrunner must implement the "get logger" method.')
   }
 
   async run () {
-    if (runningTask) {
-      logger.info('---- Queuing task "%s", task "%s" currently running.', this.friendlyName, runningTask.friendlyName)
+    const logger = this.logger
+
+    if (taskRunning) {
+      logger.info('---- Queuing task "%s", task "%s" currently running.', this.friendlyName, taskRunning.friendlyName)
 
       queue.add(this)
       return
     }
 
-    runningTask = this
+    taskRunning = this
 
-    this._runs++
+    this.runCount++
     this._startTime = new Date()
 
-    logger.info('>>>> Running periodic task "%s". Run number #%s.', this.friendlyName, this._runs)
+    logger.info('>>>> Running periodic task "%s". Run number %s.', this.friendlyName, this.runCount)
+
+    let hadError = false
 
     try {
-      await this.task(await TaskStats.maxStartedAt(this.friendlyName))
-      await this.scheduleNextRun()
-    } catch (error) {
-      logger.error('Error while running periodic task "%s".', this.friendlyName)
-      logger.verbose(error.stack)
+      await this.task()
+    } catch (err) {
+      logger.error('Error while running periodic task "%s": %s.', this.friendlyName, err.message)
+      logger.debug(err.stack)
 
-      await this.scheduleNextRun(5, true)
+      hadError = true
     }
+
+    this.scheduleNextRun(hadError ? 5 : null, hadError)
   }
 
-  async scheduleNextRun (overrideTimeout = null, hadError = false) {
-    const duration = getDuration(this._startTime)
-    const timeout = overrideTimeout || this.interval
+  scheduleNextRun (overrideTimeout = null, hadError = false) {
+    const now = new Date()
+    const elapsed = getDuration(this._startTime, now)
+    const timeout = (overrideTimeout || this.interval)
 
-    logger.info('>>>> Periodic task "%s" run #%s %s (took %s sec). Running again in %s sec.', this.friendlyName, this._runs, hadError ? 'aborted' : 'completed', duration, timeout)
+    this.logger.info('>>>> Periodic task "%s" run #%s %s (took %s sec). Running again in %s sec.', this.friendlyName, this.runCount, hadError ? 'aborted' : 'completed', elapsed, timeout)
 
     setTimeout(this.run.bind(this), timeout * 1000)
 
-    runningTask = null
-
-    try {
-      await this.saveStats(duration, hadError)
-    } catch (e) {
-      logger.warn('Error while attempting to save TaskStats: %s', e.message)
-    }
+    taskRunning = null
 
     if (queue.size > 0) {
       const queuedTask = [ ...queue ].shift()
+
+      queuedTask.run()
       queue.delete(queuedTask)
-
-      await queuedTask.run()
     }
-  }
 
-  getTaskStats (duration, hadError) {
-    return {
-      task: this.friendlyName,
-      duration,
+    this.saveStats({
       error: hadError,
-      startedAt: this._startTime
-    }
+      task: this.friendlyName,
+      start: this._startTime,
+      end: now,
+      elapsed
+    })
   }
 
-  async saveStats (duration, hadError) {
-    logger.debug('%s.saveStats not implemented', this.constructor.name)
+  saveStats (stats) {
+    this.logger.debug('"saveStats" method not implemented in class %s.', this.constructor.name)
   }
 }
-
-export default AbstractTaskRunner
