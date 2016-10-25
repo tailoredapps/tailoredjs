b50f3098e52dd2bd9eae601b985fc6dbe93f8d62
Michael Smesnik <ms@tailored-apps.com>
2016-10-25 11:20:06 +0200

    add "logLevel" argument to Taskrunner

--- src/taskrunner.js
+++ src/taskrunner.js
@@ -7,22 +7,21 @@ let taskRunning = false
 let queue = new Set()
 
 export default class Taskrunner {
-  constructor (task, interval, logger, friendlyName = 'PeriodicTask') {
+  constructor (task, interval, logger, friendlyName = 'PeriodicTask', logLevel = 'info') {
     this.task = task
     this.interval = interval
     this.friendlyName = friendlyName
 
     this.logger = logger
     this.runCount = 0
+    this.outputLog = this.logger[logLevel].bind(this.logger)
 
-    this.logger.info('++++ Registering new periodic task "%s", running every %s sec.', this.friendlyName, this.interval)
+    this.outputLog('++++ Registering new periodic task "%s", running every %s sec.', this.friendlyName, this.interval)
   }
 
   async run () {
-    const logger = this.logger
-
     if (taskRunning) {
-      logger.info('---- Queuing task "%s", task "%s" currently running.', this.friendlyName, taskRunning.friendlyName)
+      this.outputLog('---- Queuing task "%s", task "%s" currently running.', this.friendlyName, taskRunning.friendlyName)
 
       queue.add(this)
       return
@@ -33,15 +32,15 @@ export default class Taskrunner {
     this.runCount++
     this._startTime = new Date()
 
-    logger.info('>>>> Running periodic task "%s". Run number %s.', this.friendlyName, this.runCount)
+    this.outputLog('>>>> Running periodic task "%s". Run number %s.', this.friendlyName, this.runCount)
 
     let hadError = false
 
     try {
       await this.task()
     } catch (err) {
-      logger.error('Error while running periodic task "%s": %s.', this.friendlyName, err.message)
-      logger.debug(err.stack)
+      this.logger.error('Error while running periodic task "%s": %s.', this.friendlyName, err.message)
+      this.logger.debug(err.stack)
 
       hadError = true
     }
@@ -54,7 +53,7 @@ export default class Taskrunner {
     const elapsed = getDuration(this._startTime, now)
     const timeout = (overrideTimeout || this.interval)
 
-    this.logger.info('>>>> Periodic task "%s" run #%s %s (took %s sec). Running again in %s sec.', this.friendlyName, this.runCount, hadError ? 'aborted' : 'completed', elapsed, timeout)
+    this.outputLog('>>>> Periodic task "%s" run #%s %s (took %s sec). Running again in %s sec.', this.friendlyName, this.runCount, hadError ? 'aborted' : 'completed', elapsed, timeout)
 
     setTimeout(this.run.bind(this), timeout * 1000)
 
