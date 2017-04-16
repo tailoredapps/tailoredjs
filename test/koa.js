24c5d86a7228bedddbf36521a65561683475880d
Michael Smesnik <ms@tailored-apps.com>
2017-04-17 00:49:44 +0200

    implement cacheLifetime koa middleware

--- test/koa.js
+++ test/koa.js
@@ -1,12 +1,14 @@
 /* eslint-env mocha */
 const { expect } = require('chai')
+const moment = require('moment')
 
 const k = require('../lib/koa-middleware')
 
 describe('koa related functionality', function () {
+  const noop = () => undefined
+  const fauxLogger = { error: noop, verbose: noop, debug: noop }
+
   describe('error handler middleware', function () {
-    const noop = function () { }
-    const fauxLogger = { error: noop, debug: noop }
     const getThrower = (err) => async () => {
       throw err
     }
@@ -91,6 +93,65 @@ describe('koa related functionality', function () {
 
       expect(ctx).to.include.all.keys([ 'status', 'body' ])
       expect(ctx.body).to.equal('message')
+    })
+  })
+
+  describe('cache lifetime middleware', function () {
+    const fauxCtx = () => {
+      let headers = {}
+
+      return {
+        state: {},
+        set: (key, val) => {
+          headers[key] = val
+        },
+        get headers () {
+          return headers
+        }
+      }
+    }
+    const handleCacheLifetime = k.cacheLifetime(fauxLogger)
+
+    it('provides a setCacheLifetime function on context state', async function () {
+      let ctx = fauxCtx()
+
+      await handleCacheLifetime(ctx, () => undefined)
+
+      expect(ctx.state.setCacheLifetime).to.be.a('function')
+    })
+
+    it('does not set cache-control headers unless specified', async function () {
+      let ctx = fauxCtx()
+
+      await handleCacheLifetime(ctx, () => undefined)
+
+      expect(ctx.headers).to.not.include.keys([ 'Cache-control' ])
+    })
+
+    it('sets Cache-control header', async function () {
+      let ctx = fauxCtx()
+
+      await handleCacheLifetime(ctx, () => {
+        const { setCacheLifetime } = ctx.state
+
+        setCacheLifetime(1)
+      })
+
+      expect(ctx.headers).to.include.keys([ 'Cache-control' ])
+      expect(ctx.headers['Cache-control']).to.equal('max-age=1')
+    })
+
+    it('sets Expires header', async function () {
+      let ctx = fauxCtx()
+
+      await handleCacheLifetime(ctx, () => {
+        const { setCacheLifetime } = ctx.state
+
+        setCacheLifetime(1, true)
+      })
+
+      expect(ctx.headers).to.include.keys([ 'Expires' ])
+      expect(ctx.headers['Expires']).to.equal(`${moment().locale('en').utc().add(1, 'second').format('ddd, DD MMM YYYY HH:mm:ss')} GMT`)
     })
   })
 })
