f474e648ad42dd21ca1b00972ac47d4ce31ddb1b
Michael Smesnik <ms@tailored-apps.com>
2017-10-18 11:08:02 +0200

    remove logger module

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // This file exports various things from other modules, more or less arbitrarily (based on usage patterns, really)
 const { getConnector } = require('./lib/api-connector')
 const koaMiddleware = require('./lib/koa-middleware')
-const { createLogger } = require('./lib/logger')
 const { Taskrunner } = require('./lib/taskrunner')
 const util = require('./lib/util')
 
 module.exports = {
   getConnector,
   koaMiddleware,
-  createLogger,
   Taskrunner,
   util
 }
