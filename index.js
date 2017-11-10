bd4df04f8b0c32a0b32a0ddfb7b7cb64724a89f9
Michael Smesnik <ms@tailored-apps.com>
2017-11-10 15:49:51 +0100

    use default export for taskrunner

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // This file exports various things from other modules, more or less arbitrarily (based on usage patterns, really)
 const { getConnector } = require('./lib/api-connector')
 const koaMiddleware = require('./lib/koa-middleware')
-const { Taskrunner } = require('./lib/taskrunner')
+const Taskrunner = require('./lib/taskrunner')
 const util = require('./lib/util')
 
 module.exports = {
